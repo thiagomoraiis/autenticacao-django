0f338f61c1e528ef450984ed9b1aa6e6f2b686be
thiagomoraiis <thiagomorais2605@gmail.com>
2023-04-22 10:23:24 -0300

    documentando

--- usuarios/views.py
+++ usuarios/views.py
@@ -12,17 +12,22 @@ def index(request):
     context = {'user':user}
     return render(request, 'index.html', context)
 
+
+#decorador que sinaliza que a pagina so pode ser acessada
+#caso o usuario esteja logado
 @login_required
 def pagina1(request):
-    if request.user.is_authenticated:
-        pass
     return render(request, 'pag1.html')
 
+#decorador que sinaliza que a pagina so pode ser acessada
+#caso o usuario esteja logado
 @login_required
 def pagina2(request):
     return render(request, 'pag2.html')
 
 def cadastro(request):
+    #verifica se o metodo da requisição é igual a POST,
+    #se for, ele obtem o valor de cada campo via template
     if request.method == 'POST':
         first_name = request.POST.get('first_name')
         last_name = request.POST.get('last_name')
@@ -32,11 +37,14 @@ def cadastro(request):
         data_nascimento = request.POST.get('data_nascimento')
         idade = request.POST.get('idade')
 
-        user = CustomUser.objects.filter(username=username).first()
+        #verifica se ja existe um usuario com esse mesmo nome
+        user = CustomUser.objects.filter(username=username, email=email).first()
 
+        #caso exista, ele irá exibir uma mensagem de erro no template
         if user:
             messages.error(request, 'Já existe um usuario com esse nome! Tente outro')
 
+        #se o usuario não existir, ele irá criar um com os valores obtidos no template e salva-lo
         novo_usario = CustomUser.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password,data_nascimento=data_nascimento, idade=idade)
         novo_usario.save()
         messages.success(request, 'Usuario cadastrado!')
@@ -44,22 +52,30 @@ def cadastro(request):
     else:
         return render(request, 'cadastro.html')
     
-    
+
 def login(request):
+    #verifica se o metodo da requisição é POST, se for, recebe o valor dos respectivos campos
+    #do input no template
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
 
+        #caso os valores do input sejam iguais ao de um usario cadastrado, ele o autentica
         user = authenticate(username=username, password=password)
-
+        
+        #caso existe um usuario com esse nome e senha, ele irá se logar e sera redirecionado para
+        #a pagina a pagina 1
         if user:
             login_django(request, user)
             return redirect('pag1')
         else:
+            #caso não exista esse usuario, será mostrado uma mensagem de erro no template
             messages.error(request, 'Essa pagina não existe')
     else:
+        #retorna a renderização da pagina login, caso o metodo da requisição não seja POST
         return render(request, 'login.html')
 
 def logout_django(request):
+    #a função logout recebe uma request que desloga o usuario e o redireciona para uma pagina de login
     logout(request)
     return redirect('login')
